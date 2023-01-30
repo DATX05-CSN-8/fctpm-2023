c7550c8a8f49dd1c4db0d5ff82a82d89f32dcc85
vltmn <melkerveltman@gmail.com>
2023-01-30 13:40:12 +0100

    add input to keep the vm running, minimize vm config

--- vm-image/init/init.c
+++ vm-image/init/init.c
@@ -1,4 +1,6 @@
 #include <sys/io.h>
+#include <string.h>
+#include <stdlib.h>
 #include <stdio.h>
 
 /*
@@ -8,11 +10,22 @@
 
 #define PORT_FC     0x03f0
 #define PORT_FC_VAL 123
+#define INP_LEN 16
 
 int main(void)
 {
     int r;
+    char inp[INP_LEN];
 
+    while(1) {
+        if (fgets(inp, INP_LEN, stdin) == NULL) {
+            fprintf(stderr, "Error getting input data\n.");
+            exit(0);
+        }
+        inp[strcspn(inp, "\n")] = 0;
+        if (!strcmp(inp, "exit"))
+            break;
+    }
     r = ioperm(PORT_FC, 1, 1);
     if (r) {
         fprintf(stderr, "Error setting up port access to 0x%x, quitting\n", PORT_FC);
