d8021e1901899933c182d01249be2e3fab216a54
vltmn <melkerveltman@gmail.com>
2023-02-16 11:39:14 +0100

    add argument parser for api socket

--- modules/orchestrator/cmd/orchestrator/orchestrator.go
+++ modules/orchestrator/cmd/orchestrator/orchestrator.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+)
+
 func main() {
-	println("Hello world!")
+	var apiSock = flag.String("api-sock", "/tmp/fctpm-orchestrator", "File path to the socket that should be listened to")
+	flag.Parse()
+	fmt.Println("api socket has value ", *apiSock)
 }
