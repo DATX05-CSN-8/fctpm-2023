6c6ea9a4107e949cf6851adb65d5c546aaf079f8
vltmn <melkerveltman@gmail.com>
2023-02-16 16:07:43 +0100

    Added create function for vm info model

--- modules/orchestrator/cmd/orchestrator/orchestrator.go
+++ modules/orchestrator/cmd/orchestrator/orchestrator.go
@@ -12,6 +12,7 @@ import (
 
 func main() {
 	apiSock := flag.String("api-sock", "/tmp/fctpm-orchestrator", "File path to the socket that should be listened to")
+	dbPath := flag.String("db-path", "test.db", "File path to the db file to be used for sqlite")
 	flag.Parse()
 
 	err := removeFileIfExists(*apiSock)
@@ -19,11 +20,14 @@ func main() {
 		panic("Could not remove socket file")
 	}
 
-	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open(*dbPath), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to db")
 	}
 	db.AutoMigrate(&vminfo.VMInfo{})
+
+	// TODO use this repository in the service
+	_ = vminfo.NewRepository(db)
 
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
--- modules/orchestrator/internal/vminfo/model.go
+++ modules/orchestrator/internal/vminfo/model.go
@@ -1,6 +1,11 @@
 package vminfo
 
-import "time"
+import (
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+)
 
 type Status int64
 
@@ -15,4 +20,17 @@ type VMInfo struct {
 	StartTime time.Time
 	EndTime   time.Time
 	Status    Status
+}
+
+func NewVMInfo(startTime time.Time) VMInfo {
+	return VMInfo{
+		Id:        uuid.NewString(),
+		StartTime: startTime,
+		EndTime:   time.UnixMilli(0),
+		Status:    Running,
+	}
+}
+
+func (m *VMInfo) String() string {
+	return fmt.Sprintf("Id: %s, Status: %d, Start: %s, End: %s", m.Id, m.Status, m.StartTime.String(), m.EndTime.String())
 }
