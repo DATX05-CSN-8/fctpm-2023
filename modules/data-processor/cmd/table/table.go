d65fe77fc288f7073346da41ae03e369c438bd80
vltmn <melkerveltman@gmail.com>
2023-05-09 14:22:01 +0200

    add indexed p95 column to boot time table

--- modules/data-processor/cmd/table/table.go
+++ modules/data-processor/cmd/table/table.go
@@ -17,8 +17,9 @@ func main() {
 	flag.Parse()
 
 	tabledata := make([][]string, 1)
-	tabledata[0] = []string{"Type", "Min", "Max", "Average", "p95", "Standard Deviation"}
-	for _, d := range inputDatas {
+	tabledata[0] = []string{"Type", "Min", "Max", "Average", "p95", "p95 Index", "Standard Deviation"}
+	var baselineP95 float64
+	for i, d := range inputDatas {
 		parsed, err := csvreader.ReadCsvFile(d.File, csvreader.OutputDataRowParser)
 		if err != nil {
 			panic(err)
@@ -29,6 +30,13 @@ func main() {
 		max := deltas[len(deltas)-1]
 		avg := stat.Mean(deltas, nil)
 		p95 := stat.Quantile(0.95, stat.Empirical, deltas, nil)
+		var p95idx float64
+		if i == 0 {
+			p95idx = 1
+			baselineP95 = p95
+		} else {
+			p95idx = p95 / baselineP95
+		}
 		std := stat.StdDev(deltas, nil)
 		format := "%.2f"
 		tabledata = append(tabledata, []string{
@@ -37,6 +45,7 @@ func main() {
 			fmt.Sprintf(format, max),
 			fmt.Sprintf(format, avg),
 			fmt.Sprintf(format, p95),
+			fmt.Sprintf(format, p95idx),
 			fmt.Sprintf(format, std),
 		})
 	}
