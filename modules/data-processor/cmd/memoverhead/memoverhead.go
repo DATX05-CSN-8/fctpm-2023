71b18db3a71cc3b4ad82ac3ea286cbb62accfbfa
vltmn <melkerveltman@gmail.com>
2023-05-11 11:58:24 +0200

    update mem overhead y axis

--- modules/data-processor/cmd/memoverhead/memoverhead.go
+++ modules/data-processor/cmd/memoverhead/memoverhead.go
@@ -50,8 +50,8 @@ func main() {
 	// p.Y.Tick.Marker = MyTicker{width: 0.05, labelwidth: 0.2, labelfmt: "%.1f"}
 	p.X.Min = min - float64(extraSize)
 	p.X.Max = max + float64(extraSize)
-	p.Y.Min = 4
-	p.Y.Max = 15
+	p.Y.Min = 2
+	p.Y.Max = 4
 
 	err = p.Save(4*vg.Inch, 4*vg.Inch, "out."+*outputtype)
 	if err != nil {
